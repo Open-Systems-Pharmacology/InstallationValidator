90c7cb69433af5069e3ac18ffa14ce2739c11c97
rwmcintosh <rob@robertmcintosh.ca>
2017-04-28 15:34:08 -0400

    remove file not referenced in project

--- src/OSPSuite.InstallationValidator.Core/Reporting/InstallationValidationResultReporter.cs
+++ src/OSPSuite.InstallationValidator.Core/Reporting/InstallationValidationResultReporter.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using OSPSuite.Infrastructure.Reporting;
-using OSPSuite.InstallationValidator.Core.Assets;
 using OSPSuite.InstallationValidator.Core.Domain;
 using OSPSuite.TeXReporting.Items;
 
@@ -8,13 +7,13 @@ namespace OSPSuite.InstallationValidator.Core.Reporting
 {
    public class InstallationValidationResultReporter : OSPSuiteTeXReporter<InstallationValidationResult>
    {
-      public override IReadOnlyCollection<object> Report(InstallationValidationResult objectToReport, OSPSuiteTracker buildTracker)
+      public override IReadOnlyCollection<object> Report(InstallationValidationResult installationValidationResult, OSPSuiteTracker buildTracker)
       {
          var validationResultsChapter = new Chapter(Assets.Reporting.InstallationValidationResults);
          buildTracker.Track(validationResultsChapter);
          return new List<object>
          {
-            validationResultsChapter, objectToReport.RunSummary, objectToReport.ComparisonResult
+            validationResultsChapter, installationValidationResult.RunSummary, installationValidationResult.ComparisonResult
          };
       }
 
