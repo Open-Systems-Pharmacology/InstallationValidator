68d1581231a11489d821be970ef2daee0d2ba200
rwmcintosh <rob@robertmcintosh.ca>
2017-05-02 11:39:54 -0400

    basic charts added

--- src/OSPSuite.InstallationValidator.Core/Reporting/OutputComparisonResultTeXBuilder.cs
+++ src/OSPSuite.InstallationValidator.Core/Reporting/OutputComparisonResultTeXBuilder.cs
@@ -1,34 +1,135 @@
+using System.Collections.Generic;
+using FluentNHibernate.Utils;
+using OSPSuite.Core.Chart;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Data;
+using OSPSuite.Core.Domain.UnitSystem;
+using OSPSuite.Core.Extensions;
 using OSPSuite.Infrastructure.Reporting;
-using OSPSuite.InstallationValidator.Core.Assets;
 using OSPSuite.InstallationValidator.Core.Domain;
+using OSPSuite.InstallationValidator.Core.Extensions;
 using OSPSuite.TeXReporting.Builder;
 using OSPSuite.TeXReporting.Items;
+using OSPSuite.Utility;
 
 namespace OSPSuite.InstallationValidator.Core.Reporting
 {
    public class OutputComparisonResultTeXBuilder : ValueComparisonResultTeXBuilder<OutputComparisonResult>
    {
       private readonly ITeXBuilderRepository _builderRepository;
+      private readonly IOutputComparisonResultToCurveChartMapper _outputComparisonResultToCurveChartMapper;
 
-      public OutputComparisonResultTeXBuilder(ITeXBuilderRepository builderRepository)
+      public OutputComparisonResultTeXBuilder(ITeXBuilderRepository builderRepository, IOutputComparisonResultToCurveChartMapper outputComparisonResultToCurveChartMapper)
       {
          _builderRepository = builderRepository;
+         _outputComparisonResultToCurveChartMapper = outputComparisonResultToCurveChartMapper;
       }
       public override void Build(OutputComparisonResult outputToReport, OSPSuiteTracker buildTracker)
       {
-         _builderRepository.Report(new object[]
+         var objectsToReport = new List<object>
          {
             new LineBreak(),
             Assets.Reporting.OutputComparisonValidation,
             new LineBreak(), ValidationMessageFor(outputToReport),
             new LineBreak(), outputPathFor(outputToReport),
-            new LineBreak(), DeviationFor(outputToReport)
-         }, buildTracker);
+            new LineBreak(), DeviationFor(outputToReport),
+
+         };
+
+         if (canCreateChartFor(outputToReport))
+         {
+            chartsFor(outputToReport).Each(chart =>
+            {
+               objectsToReport.AddRange(new object[] { new LineBreak(), chart});
+            });
+         }
+
+         _builderRepository.Report(objectsToReport, buildTracker);
+      }
+
+      private static bool canCreateChartFor(OutputComparisonResult outputToReport)
+      {
+         return !(outputToReport.Output1.IsNullOutput() || outputToReport.Output2.IsNullOutput());
+      }
+
+      private IEnumerable<CurveChart> chartsFor(OutputComparisonResult outputComparisonResult)
+      {
+         return _outputComparisonResultToCurveChartMapper.MapFrom(outputComparisonResult);
       }
 
       private static string outputPathFor(OutputComparisonResult outputToReport)
       {
          return $"{Assets.Reporting.OutputPath}: {outputToReport.Path}";
+      }
+   }
+
+   public interface IOutputComparisonResultToCurveChartMapper : IMapper<OutputComparisonResult, IEnumerable<CurveChart>>
+   {
+   }
+
+   public class OutputComparisonResultToCurveChartMapper : IOutputComparisonResultToCurveChartMapper
+   {
+      private readonly IOutputResultToDataRepositoryMapper _outputResultToDataRepositoryMapper;
+      private readonly IDimensionFactory _dimensionFactory;
+
+      public OutputComparisonResultToCurveChartMapper(IOutputResultToDataRepositoryMapper outputResultToDataRepositoryMapper, IDimensionFactory dimensionFactory)
+      {
+         _outputResultToDataRepositoryMapper = outputResultToDataRepositoryMapper;
+         _dimensionFactory = dimensionFactory;
+      }
+
+      public IEnumerable<CurveChart> MapFrom(OutputComparisonResult outputComparisonResult)
+      {
+         var dataRepository1 = _outputResultToDataRepositoryMapper.MapFrom(outputComparisonResult.Output1);
+         var dataRepository2 = _outputResultToDataRepositoryMapper.MapFrom(outputComparisonResult.Output2);
+
+         var linearCurveChart = new CurveChart {DefaultYAxisScaling = Scalings.Linear};
+         addToCurveChart(linearCurveChart, dataRepository1, dataRepository2);
+
+         var logCurveChart = new CurveChart { DefaultYAxisScaling = Scalings.Log };
+         addToCurveChart(logCurveChart, dataRepository1, dataRepository2);
+         return new []{ linearCurveChart, logCurveChart};
+      }
+
+      private void addToCurveChart(CurveChart curveChart, DataRepository dataRepository1, DataRepository dataRepository2)
+      {
+         curveChart.AddCurvesFor(dataRepository1, x => x.PathAsString, _dimensionFactory, (column, curve) => { curve.Name = "Output 1"; });
+         curveChart.AddCurvesFor(dataRepository2, x => x.PathAsString, _dimensionFactory, (column, curve) => { curve.Name = "Output 2"; });
+      }
+   }
+
+   public interface IOutputResultToDataRepositoryMapper : IMapper<OutputResult, DataRepository>
+   {
+   }
+
+   public class OutputResultToDataRepositoryMapper : IOutputResultToDataRepositoryMapper
+   {
+      private readonly IDimensionFactory _dimensionFactory;
+
+      public OutputResultToDataRepositoryMapper(IDimensionFactory dimensionFactory)
+      {
+         _dimensionFactory = dimensionFactory;
+      }
+
+      public DataRepository MapFrom(OutputResult outputResult)
+      {
+         var repository = new DataRepository();
+
+         var timeGrid = new BaseGrid("Time", _dimensionFactory.GetDimension("Time"))
+         {
+            Values = outputResult.Times
+         };
+
+
+         var outputColumn = new DataColumn("Values", _dimensionFactory.NoDimension, timeGrid)
+         {
+            Values = outputResult.Values
+         };
+
+
+         repository.Add(outputColumn);
+
+         return repository;
       }
    }
 }
--- tests/OSPSuite.InstallationValidator.Tests/IntegrationTests/ReportingSpecs.cs
+++ tests/OSPSuite.InstallationValidator.Tests/IntegrationTests/ReportingSpecs.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
@@ -9,6 +10,7 @@ using OSPSuite.Core.Services;
 using OSPSuite.InstallationValidator.Core.Domain;
 using OSPSuite.TeXReporting;
 using OSPSuite.Utility;
+using OSPSuite.Utility.Extensions;
 
 namespace OSPSuite.InstallationValidator.IntegrationTests
 {
@@ -64,7 +66,7 @@ namespace OSPSuite.InstallationValidator.IntegrationTests
 
          var outputFileComparisonResult = new OutputFileComparisonResult("a file.txt", "a folder", "another folder");
          var timeFileComparisonResult = new OutputFileComparisonResult("a file.txt", "a folder", "another folder");
-         var outputComparisonResult = new OutputComparisonResult("the path", ValidationState.Invalid, "the message") { Deviation = 44.0 };
+         var outputComparisonResult = createOutputDeviationFailureResult();
          var timeComparisonResult = new TimeComparisonResult(ValidationState.Invalid, "the time message") { Deviation = 1.0 };
          timeFileComparisonResult.TimeComparison = timeComparisonResult;
          timeFileComparisonResult.AddOutputComparison(new OutputComparisonResult("valid", ValidationState.Valid, ""));
@@ -96,5 +98,27 @@ namespace OSPSuite.InstallationValidator.IntegrationTests
          };
          CreateReportAndValidate(installationValidationResult, "validationReport");
       }
+
+      private static OutputComparisonResult createOutputDeviationFailureResult()
+      {
+         var outputDeviationFailureResult = new OutputComparisonResult("the path", ValidationState.Invalid, "the message")
+         {
+            Deviation = 44.0,
+            Output1 = new OutputResult(getTimes(), getValues(x => 2 * x)),
+            Output2 = new OutputResult(getTimes(), getValues(x => x))
+         };
+         return outputDeviationFailureResult;
+      }
+
+      private static float[] getValues(Func<float, float> transform)
+      {
+         return getTimes().Select(transform).ToArray();
+      }
+
+      private static float[] getTimes()
+      {
+         return new[] {0.0f, 1.1f, 1.2f, 1.3f, 1.4f};
+      }
+
    }
 }
