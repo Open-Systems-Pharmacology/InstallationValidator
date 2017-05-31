7efa19456709eb636884d45883f4e40a0d65fc1c
Michael Sevestre <michael@design2code.ca>
2017-05-31 10:20:59 -0400

    Fixes #109 bump to version 7.2.0

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("InstallationValidator")]
 [assembly: AssemblyCopyright("Copyright © 2017 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("7.1.0.0")]
-[assembly: AssemblyFileVersion("7.1.0.0")]
+[assembly: AssemblyVersion("7.2.0.0")]
+[assembly: AssemblyFileVersion("7.2.0.0")]
 [assembly: InternalsVisibleTo("InstallationValidator.Tests")]
