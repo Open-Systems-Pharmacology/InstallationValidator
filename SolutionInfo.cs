afe524857ea3487a0c29ed266a17ced1ae7890e0
Michael Sevestre <michael@design2code.ca>
2019-04-26 16:10:04 -0400

    Fixes #179 create version 750

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("Open Systems Pharmacology Community")]
 [assembly: AssemblyProduct("InstallationValidator")]
-[assembly: AssemblyCopyright("Copyright © 2017 - Open Systems Pharmacology Community")]
+[assembly: AssemblyCopyright("Copyright © 2002-2019 - Open Systems Pharmacology Community")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("7.4.0.0")]
-[assembly: AssemblyFileVersion("7.4.0.0")]
+[assembly: AssemblyVersion("7.5.0.0")]
+[assembly: AssemblyFileVersion("7.5.0.0")]
 [assembly: InternalsVisibleTo("InstallationValidator.Tests")]
