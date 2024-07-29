72d6092a4dd9d0b0e9b8075f72a27783970aed4b
Michael Sevestre <michael@design2code.ca>
2024-07-29 05:41:18 -0400

    Fixes #269 (#276)

--- src/InstallationValidator.Core/Services/BatchOutputLoader.cs
+++ src/InstallationValidator.Core/Services/BatchOutputLoader.cs
@@ -1,6 +1,9 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json;
 using OSPSuite.Core.Batch;
+using OSPSuite.Core.Domain.UnitSystem;
 
 namespace InstallationValidator.Core.Services
 {
@@ -11,15 +14,95 @@ namespace InstallationValidator.Core.Services
 
    public class BatchOutputLoader : IBatchOutputLoader
    {
+      private readonly IDimensionFactory _dimensionFactory;
+
+      public BatchOutputLoader(IDimensionFactory dimensionFactory)
+      {
+         _dimensionFactory = dimensionFactory;
+      }
+
       public BatchSimulationExport LoadFrom(string fileName)
       {
          var serializer = new JsonSerializer();
 
-         using (var sr = new StreamReader(fileName))
-         using (var reader = new JsonTextReader(sr))
+         try
          {
-            return serializer.Deserialize<BatchSimulationExport>(reader);
+            using (var sr = new StreamReader(fileName))
+            using (var reader = new JsonTextReader(sr))
+            {
+               return serializer.Deserialize<BatchSimulationExport>(reader);
+            }
+         }
+         catch (Exception e)
+         {
+            //try to load the old way and convert to new way. Temporary for v11
+            //required to load again since the reader was consumed
+            using (var sr = new StreamReader(fileName))
+            using (var reader = new JsonTextReader(sr))
+            {
+               var oldBatchSimulationExport = serializer.Deserialize<OldBatchSimulationExport>(reader);
+               return mapToBatchSimulationExport(oldBatchSimulationExport);
+            }
          }
       }
+
+      private BatchSimulationExport mapToBatchSimulationExport(OldBatchSimulationExport oldBatchSimulationExport)
+      {
+         //convert to batch simulation export
+         var batchSimulationExport = new BatchSimulationExport
+         {
+            Name = oldBatchSimulationExport.Name,
+            AbsTol = oldBatchSimulationExport.AbsTol,
+            RelTol = oldBatchSimulationExport.RelTol,
+            Times = new BatchValues
+            {
+               Unit = "h",
+               Values = oldBatchSimulationExport.Times,
+               Dimension = "Time"
+            },
+            ParameterValues = oldBatchSimulationExport.ParameterValues,
+         };
+
+         foreach (var outputValues in oldBatchSimulationExport.OutputValues)
+         {
+            var dimension = _dimensionFactory.Dimension(outputValues.Dimension);
+            batchSimulationExport.OutputValues.Add(new BatchOutputValues
+            {
+               Path = outputValues.Path,
+               Values = outputValues.Values,
+               Dimension = outputValues.Dimension,
+               Unit = dimension.DefaultUnitName,
+            });
+         }
+
+        
+
+         return batchSimulationExport;
+      }
    }
+
+   class OldBatchSimulationExport
+   {
+      public string Name { get; set; }
+
+      /// <summary>
+      ///    Time Array used in the simulation
+      /// </summary>
+      public float[] Times { get; set; }
+
+      /// <summary>
+      ///    Absolute tolerance used in the simulation
+      /// </summary>
+      public double AbsTol { get; set; }
+
+      /// <summary>
+      ///    Relative tolerance used in the simulation
+      /// </summary>
+      public double RelTol { get; set; }
+
+      public List<BatchOutputValues> OutputValues { get; set; } = new List<BatchOutputValues>();
+      public List<ParameterValue> ParameterValues { get; set; } = new List<ParameterValue>();
+   }
+
+
 }
